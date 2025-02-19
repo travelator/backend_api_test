3665d17fc1b13f4b1764e8a57d713c9a1e056cf3
Tim Jones personal laptop <timspevackjones@gmail.com>
2025-02-19 12:54:23 +0000

    feature: updated the dadta for get_rate_info. Added a new itinerary route

--- app.py
+++ app.py
@@ -9,39 +9,59 @@ CORS(app, origins=["http://localhost:5173"])
 @app.route('/api/rate-info', methods=['GET'])
 def get_rate_info():
     # Replace this with actual data from your backend or database
-    data = [
+    rate_data = [
         {
             "title": "Visit the British Museum",
             "description": "Explore a vast collection of art and antiquities from around the world at the British Museum, one of London's most iconic cultural institutions.",
             "image_link": "https://www.visitlondon.com/-/media/images/london/visit/things-to-do/sightseeing/london-attractions/british-museum/british-museum-2023-images/british-museum-in-london.jpg?h=360&w=640&rev=9705af40409148acb39ad44319df9111&hash=33A9991BE2DE2CAF1B052471CD5ACEBE",
+            "price": 0,
+            "theme": 'Culture',
             "id": 1
         },
         {
             "title": "Ride the London Eye",
             "description": "Experience panoramic views of London's skyline from the London Eye, a giant Ferris wheel situated on the South Bank of the River Thames.",
             "image_link": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/London-Eye-2009.JPG/800px-London-Eye-2009.JPG",
+            "price": 60,
+            "theme": 'Culture',
             "id": 2
         },
         {
             "title": "Explore Hyde Park",
             "description": "Enjoy a leisurely stroll, picnic, or boating in Hyde Park, one of London's largest and most famous green spaces.",
             "image_link": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Hyde_Park_London_from_the_air.jpg/640px-Hyde_Park_London_from_the_air.jpg",
+            "price": 0,
+            "theme": 'Relaxation',
             "id": 3
         },
         {
             "title": "Tour the Tower of London",
             "description": "Discover the rich history of the Tower of London, a historic castle on the north bank of the River Thames, known for housing the Crown Jewels.",
             "image_link": "https://www.cuddlynest.com/blog/wp-content/uploads/2022/12/visiting-the-tower-of-london-scaled.jpg",
+            "price": 30,
+            "theme": 'Culture',
             "id": 4
         },
         {
             "title": "See a West End Theatre Show",
             "description": "Experience world-class theatre productions in London's West End, renowned for its vibrant performing arts scene.",
             "image_link": "https://images.ctfassets.net/6pezt69ih962/5emZ299oIHRkgEgovv4sqd/74b5f91b5ceaa2893a582a005a5bbe72/004-Andy-Paradise-min.jpg",
+            "theme": 'Entertainment',
+            "price": 80,
             "id": 5
         }
     ]
-    return jsonify(data)
+    return jsonify(rate_data)
+
+@app.route('/api/itinerary', methods=['GET'])
+def get_itinerary_info():
+    # Replace this with actual data from your backend or database
+    itinerary_data = [
+        {
+            "status": "working",
+        },
+    ]
+    return jsonify(itinerary_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
